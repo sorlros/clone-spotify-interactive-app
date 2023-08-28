735a7a8f8c3f89114855cf89997bf7cb8a030cbf
sorlros <sorlros2047@gmail.com>
2023-08-28 16:04:08 +0900

    songs fetching and list display

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import SupabaseProvider from "@/providers/SupabaseProvider";
 import UserProvider from "@/providers/UserProvider";
 import ModalProvider from "../providers/ModalProvider";
 import ToasterProvider from "@/providers/ToasterProvider";
+import getSongsByUserId from "@/actions/getSongsByUserID";
 
 const font = Figtree({ subsets: ["latin"] });
 
@@ -14,7 +15,11 @@ export const metadata: Metadata = {
 	description: "Portfolio web-app using next.js",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export const revalidate = 0;
+
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+	const userSongs = await getSongsByUserId();
+
 	return (
 		<html lang="en">
 			<body className={font.className}>
@@ -22,7 +27,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 				<SupabaseProvider>
 					<UserProvider>
 						<ModalProvider />
-						<Sidebar>{children}</Sidebar>
+						<Sidebar songs={userSongs}>{children}</Sidebar>
 					</UserProvider>
 				</SupabaseProvider>
 			</body>
